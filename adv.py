23cdc9f8d82434959f658c87171bf60da850968e
Daniel <danstad2012@gmail.com>
2020-04-17 09:25:29 -0700

    mvp

--- adv.py
+++ adv.py
@@ -12,8 +12,8 @@ world = World()
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
-map_file = "maps/test_loop.txt"
-# map_file = "maps/test_loop_fork.txt"
+# map_file = "maps/test_loop.txt"
+map_file = "maps/test_loop_fork.txt"
 #map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
@@ -25,11 +25,36 @@ world.print_rooms()
 
 player = Player(world.starting_room)
 
-# Fill this out with directions to walk
-# traversal_path = ['n', 'n']
 n,e,w,s = 'n','e','w','s'
-loop_sol = [n,n,s,s,e,e,w,w,w,w,s,s,e,e,n]
+bt = {n:s,s:n,e:w,w:e}
 traversal_path = []
+visited = {}
+walk_balk = []
+
+#init visited
+visited[player.current_room.id] = player.current_room.get_exits()
+
+while len(visited) <  len(room_graph):
+
+
+    if player.current_room.id not in visited: #enter new room
+        visited[player.current_room.id] = player.current_room.get_exits() #add new room to visited w/ possible moves
+        visited[player.current_room.id].remove(walk_balk[-1])
+
+    if len(visited[player.current_room.id]) == 0: #hit dead end
+        prev_dir = walk_balk.pop()
+        player.travel(prev_dir)
+        traversal_path.append(prev_dir)
+
+    else: #
+        direction = visited[player.current_room.id][-1]
+
+
+        visited[player.current_room.id].pop()
+        traversal_path.append(direction)
+        walk_balk.append(bt[direction])
+        player.travel(direction)
+    print(f'path: {traversal_path} \nvisited: {visited} \nwalkbalk: {walk_balk} \n\n')
 
 
 
